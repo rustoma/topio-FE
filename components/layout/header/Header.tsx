d564a386cb489fe3d8e0576ef475d3232fd45463
rustoma <rutoma00@gmail.com>
2023-06-03 14:49:33 +0200

    feat: add header

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -1,25 +1,60 @@
+"use client";
 import React from "react";
+import clsx from "clsx";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { Container } from "@/components/container/Container";
 import { MainLogo } from "@/components/logos/MainLogo";
 import { MainMenu } from "@/components/menus/mainMenu/MainMenu";
+import { HamburgerMenu } from "@/components/menus/mobileMenu/hamburgerMenu/HamburgerMenu";
+import { MenuLink } from "@/types/menu";
 
 import "./header.style.scss";
 
+const MENU_TREE: MenuLink[] = [
+  {
+    id: "kontakt-page",
+    title: "Kontakt",
+    url: "/kontakt",
+    items: [],
+  },
+];
+
 interface HeaderProps {
-  type?: "primary" | "secondary";
+  menu: MenuLink[];
 }
 
-export const Header = ({ type = "secondary" }: HeaderProps) => {
+export const Header = ({ menu }: HeaderProps) => {
+  const pathName = usePathname();
+
   return (
-    <header className={`main-header main-header--${type}`}>
+    <header className={clsx("main-header", pathName === "/" ? "main-header--primary" : "main-header--secondary")}>
       <Container className="main-header__container">
         <div className="main-header__content-wrapper">
           <Link href="/" className="main-logo__link">
             <MainLogo className="main-logo__icon--header" />
           </Link>
-          <MainMenu />
+          <MainMenu
+            menuTree={[
+              {
+                id: "Wszystkie rankingi",
+                title: "Wszystkie rankingi",
+                items: menu,
+              },
+              ...MENU_TREE,
+            ]}
+          />
+          <HamburgerMenu
+            menuTree={[
+              {
+                id: "Wszystkie rankingi",
+                title: "Wszystkie rankingi",
+                items: menu,
+              },
+              ...MENU_TREE,
+            ]}
+          />
         </div>
       </Container>
     </header>
