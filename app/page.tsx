69de9c8c0ddc5795adfbbc179f8849365cdfd870
rustoma <rutoma00@gmail.com>
2023-06-04 07:26:05 +0200

    feat: add homepage and google analytics to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,73 @@
-import Image from 'next/image';
+import React from "react";
+import ReactMarkdown from "react-markdown";
+
+import { Container } from "@/components/container/Container";
+import MainBanner from "@/components/mainBanner/MainBanner";
+import { NewestRankings } from "@/components/newestRankings/NewestRankings";
+import QuickNav from "@/components/quickNav/QuickNav";
+
+import "./page.style.scss";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <MainBanner
+        title="Nie wiesz jaki Produkt kupić ?"
+        body="Jesteś we właściwym miejscu ! Zapewniamy wiarygodne recenzje, przewodniki kupującego i porady dotyczące wyboru najlepszych produktów."
+      />
+      <Container>
+        <div className="home__section">
+          <div className="remote-text">
+            <ReactMarkdown>{`Witaj na naszej niezwykłej stronie, gdzie czeka na Ciebie niezliczona gama rankingów produktów. 
+            
+Pragniemy Cię oczarować i pomóc w znalezieniu najlepszego produktu, który idealnie pasuje do Twoich potrzeb.
+
+Czy kiedykolwiek zastanawiałeś się, jak wiele możliwości drzemie na rynku? Jak znaleźć ten jedyny, wyjątkowy produkt, który spełni Twoje oczekiwania? Czy pragniesz odkryć coś wyjątkowego, co nada blasku Twojemu życiu?
+
+Oto miejsce, które stworzyliśmy dla Ciebie! Nasza strona pełna pasjonujących rankingów pomoże Ci w wyborze idealnego produktu. 
+
+Pragniemy, abyś doświadczył przyjemności odkrywania i zyskał pewność, że dokonujesz najlepszego wyboru.
+
+Dzięki naszym rankingom i recenzjom, pokażemy Ci świat pełen możliwości.
+
+Razem odkryjemy najwyższej jakości produkty, które spełnią Twoje marzenia. Nie zwlekaj! Rozpocznij tę fascynującą przygodę!`}</ReactMarkdown>
+          </div>
+          {/*@ts-expect-error Server Component*/}
+          <QuickNav />
         </div>
-      </div>
+        <div className="home__section">
+          <div className="remote-text">
+            <ReactMarkdown>{`## Nowości
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+Czy zastanawiałeś się kiedyś, jakie są najnowsze trendy w świecie produktów? Czy chcesz być na bieżąco z tym, co jest na topie? 
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+W naszej sekcji “Nowości” znajdziesz najświeższe rankingi produktów, które pozwolą Ci być zawsze o krok przed innymi. 
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+Dlaczego warto śledzić nasze nowości? 
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+Ponieważ dzięki temu będziesz miał dostęp do najnowszych informacji i będziesz mógł podejmować świadome decyzje zakupowe. Czy nie warto wiedzieć, co jest najlepsze na rynku? Zapraszamy do zapoznania się z naszą sekcją i odkrywania nowych trendów!`}</ReactMarkdown>
+          </div>
+          {/*@ts-expect-error Server Component*/}
+          <NewestRankings />
+        </div>
+        <div className="home__section">
+          <div className="remote-text">
+            <ReactMarkdown>{`Już teraz wkraczasz na terytorium ekscytujących rankingów, gdzie czeka na Ciebie świat pełen możliwości. 
+            
+Zaufaj nam, gdy mówimy, że warto skorzystać z naszej strony. 
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+Dlaczego? 
+
+Bo z nami odkryjesz sekrety najlepszych produktów, które spełnią Twoje oczekiwania!
+
+Czy zdarza Ci się czuć przytłoczony mnogością opcji? 
+
+Właśnie dlatego jesteśmy tutaj, aby Ci pomóc! Nasze rankingi są stworzone po to, by uprościć Ci życie i dać Ci pewność, że wybierasz najwyższej jakości produkty, które spełnią Twoje potrzeby. 
+
+Nie musisz już tracić czasu i energii na szukanie igły w stogu siana – my już to dla Ciebie zrobiliśmy!`}</ReactMarkdown>
+          </div>
+        </div>
+      </Container>
     </main>
   );
 }
