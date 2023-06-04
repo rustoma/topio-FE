21410c05e1fe927c1875abe7ffaa859815a498cb
rustoma <rutoma00@gmail.com>
2023-06-04 07:27:04 +0200

    feat: add Rank page and qucik nav

---
+++ components/quickNav/QuickNav.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Link from "next/link";
+
+import { getpageByCategories } from "@/services/page";
+
+import "./quickNav.style.scss";
+
+const QuickNav = async () => {
+  const categories = await getpageByCategories();
+
+  return (
+    <div className="quick-nav">
+      <h3 className="quick-nav__title">Szybka nawigacja</h3>
+      <div className="quick-nav__content">
+        {categories.map((category) => (
+          <div key={category.category_name} className="quick-nav__group">
+            <h4 className="quick-nav__group-title">{category.category_name}</h4>
+            <ul className="quick-nav__group-list">
+              {category.pages &&
+                category.pages.map((page) => (
+                  <li key={page.id} className="quick-nav__group-list-item">
+                    <Link href={page.slug} className="quick-nav__group-list-item-link">
+                      {page.title}
+                    </Link>
+                  </li>
+                ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuickNav;
---
+++ components/rankPage/RankPage.tsx
@@ -0,0 +1,75 @@
+"use client";
+import React from "react";
+import ReactMarkdown from "react-markdown";
+
+import { Breadcrumbs } from "@/components/breadcrumbs/Breadcrumbs";
+import { Container } from "@/components/container/Container";
+import { ProductsRanking } from "@/components/productsRanking/ProductsRanking";
+import PublicationDetails from "@/components/publicationDetails/PublicationDetails";
+import QuickComparison from "@/components/quickComparison/QuickComparison";
+import { useScroll } from "@/hooks/useScroll";
+import { Product } from "@/types/product";
+
+interface RankPageProps {
+  id: number;
+  title: string;
+  intro: string;
+  body: string;
+  CreatedAt: string;
+  UpdatedAt: string;
+  vacuumCleaners?: Product[];
+  comparisonTitle?: string;
+  comparisonSubTitle?: string;
+  rankingTitle?: string;
+}
+
+const RankPage = ({
+  title,
+  intro,
+  body,
+  UpdatedAt,
+  comparisonTitle,
+  comparisonSubTitle,
+  rankingTitle,
+  vacuumCleaners = [],
+}: RankPageProps) => {
+  const { elRefs, executeScroll, setRefs } = useScroll();
+
+  return (
+    <main className="rank-main-content">
+      <Breadcrumbs currentPageTitle={title} />
+      <Container size="xl">
+        <div className="flex">
+          <Container>
+            <h1 className="page-title">{title}</h1>
+            <PublicationDetails updateDate={UpdatedAt} />
+            <div className="remote-text">
+              <ReactMarkdown>{intro}</ReactMarkdown>
+            </div>
+
+            <QuickComparison
+              title={comparisonTitle}
+              subTitle={comparisonSubTitle}
+              products={vacuumCleaners}
+              elRefs={elRefs}
+              setRefs={setRefs}
+              executeScroll={executeScroll}
+            />
+
+            <ProductsRanking title={rankingTitle} products={vacuumCleaners} elRefs={elRefs} />
+
+            <div className="mt-12 text-justify md:mt-[120px]">
+              <div className="remote-text">
+                <ReactMarkdown>{body}</ReactMarkdown>
+              </div>
+            </div>
+            {/*<RankCards rankCards={rankCards} />*/}
+          </Container>
+          {/*{width > 1400 && <SideBar boundaryElement=".rank-main-content" bestVacuum={vacuumCleaners[0]} />}*/}
+        </div>
+      </Container>
+    </main>
+  );
+};
+
+export default RankPage;
