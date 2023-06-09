8e14cd9dce7d14af69f2a247eae46325b0c1a11b
rustoma <rutoma00@gmail.com>
2023-06-09 08:31:25 +0200

    fix: disable cache for redirects

--- services/redirect.js
+++ services/redirect.js
@@ -1,5 +1,7 @@
 const getMEXRedirects = async () => {
-  const res = await fetch(`${process.env.BACKEND_HOST}/redirects/mex`);
+  const res = await fetch(`${process.env.BACKEND_HOST}/redirects/mex`, {
+    cache: "no-store",
+  });
 
   if (!res.ok) {
     return [];
