92a6098172892118d68ac510fd9cc5f3ecca9df9
rustoma <rutoma00@gmail.com>
2023-07-22 12:40:37 +0200

    feat: add category page

---
+++ app/(dashboard)/dashboard/categories/create/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+
+import { CreateproductCategoryForm } from "@/features/dashboard/forms/createCategoryForm/CreateProductCategoryForm";
+
+const CreateProductCategory = () => {
+  return (
+    <>
+      <main>
+        <CreateproductCategoryForm />
+      </main>
+    </>
+  );
+};
+
+export default CreateProductCategory;
