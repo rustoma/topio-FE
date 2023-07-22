13f50eb69c24f540e249333acfef6f42f59af522
rustoma <rutoma00@gmail.com>
2023-07-22 12:30:14 +0200

    feat: add pageRoutes and apiRoutes consts

--- features/dashboard/const/apiRoutes.ts
+++ features/dashboard/const/apiRoutes.ts
@@ -1,5 +1,6 @@
 export const DASHBOARD_API_ROUTES = {
   rankings: "/api/v1/dashboard/pages",
+  publishRanking: (id: number) => `/api/v1/dashboard/pages/${id}/is-published`,
   products: "/api/v1/dashboard/products",
   productCategories: "/api/v1/dashboard/product-categories",
   regeneratePageTexts: (id: number) => `/api/v1/dashboard/pages/${id}/regenerate`,
