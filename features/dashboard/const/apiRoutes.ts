e2f672afad303273cffca092976e8c03dbfc8425
rustoma <rutoma00@gmail.com>
2023-07-22 06:50:55 +0200

    feat: add apiRoutes and styles for dashboard header

--- features/dashboard/const/apiRoutes.ts
+++ features/dashboard/const/apiRoutes.ts
@@ -1,4 +1,6 @@
 export const DASHBOARD_API_ROUTES = {
   rankings: "/api/v1/dashboard/pages",
   products: "/api/v1/dashboard/products",
+  productCategories: "/api/v1/dashboard/product-categories",
+  regeneratePageTexts: (id: number) => `/api/v1/dashboard/pages/${id}/regenerate`,
 };
